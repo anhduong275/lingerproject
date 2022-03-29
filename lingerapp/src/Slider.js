c26af56e3e9285dc2202a5746361586d9904b398
anhduong275 <84537455+anhduong275@users.noreply.github.com>
2022-03-29 16:03:31 -0400

    slide feature working - have not fetched new users when run out of current users.

--- lingerapp/src/Slider.js
+++ lingerapp/src/Slider.js
@@ -17,22 +17,52 @@ export default class Slider extends Component {
                 };
             }); //this.props.userSlide is in fact already array of users.
             //should i move this map to upper level, aka app.js?
+        this.state = {
+            currentUserIndex: 0,
+            currentUser: this.userSlide[0],
+        };
+    }
+
+    heartUser() {
+        this.userSlide[this.state.currentUserIndex].liked = true;
+        this.setState({
+            currentUserIndex: this.state.currentUserIndex + 1,
+        })
+    }
+
+    passUser() {
+        //do nothing to liked, aka liked == false //should we set it to false explicitly?
+        this.setState({
+            currentUserIndex: this.state.currentUserIndex + 1,
+        })
     }
 
     render() {
+        console.log(this.state.currentUserIndex);
         return (
             <div className='Slider'>
                 <div className='UserSlide'>
-                    <img src={this.userSlide[0].picture}></img>
+                    <img src={this.userSlide[this.state.currentUserIndex].picture}></img>
                 </div>
                 <div className='UserInfo'>
                     User Name, User Age
                 </div>
                 <div className='Button'>
-                    <button className='Heart'><FaHeart></FaHeart></button>
-                    <button className='Cross'><ImCross></ImCross></button>
+                    <button className='Heart' onClick={() => { this.heartUser() }}><FaHeart></FaHeart></button>
+                    <button className='Cross' onClick={() => { this.passUser() }}><ImCross></ImCross></button>
                 </div>
             </div>
         );
     }
-}
+}/*
+                {this.userSlide.map((user, index) => {
+                    return (
+                        <div className={index === this.state.currentUserIndex ? 'UserSlide Active' : 'UserSlide'} key={index}>
+                            {index === this.state.currentUserIndex && (
+                                <img src={user.picture}></img>
+                            )}
+                            {this.state.currentUserIndex}
+                        </div>
+                    );
+                })}
+                */
